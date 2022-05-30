612d63fd6dd8f5270befe925b3125fd9b9b716fd
BartWay <104139643+BartWay@users.noreply.github.com>
2022-05-30 15:54:23 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-//var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Kpayn1Titf6QIX1g9DKET-KfPehoHFKClG39t2WTEb4/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1KcPaJCEVcvDY-bXhMYdT169YKEqcSgoqyhF0UUrT0Kk/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-//var googleApiKey = 'AIzaSyCceYsSV4Yca27eo_Nk0naLGo0RxxJ3eUc';
+var googleApiKey = 'AIzaSyCceYsSV4Yca27eo_Nk0naLGo0RxxJ3eUc';
