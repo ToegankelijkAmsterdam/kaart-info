818d49a485003fdb9d82aacf8ab7a3439530af16
ToegankelijkAmsterdam <110667978+ToegankelijkAmsterdam@users.noreply.github.com>
2022-08-26 09:35:40 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1KcPaJCEVcvDY-bXhMYdT169YKEqcSgoqyhF0UUrT0Kk/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/10X8ZE-b7zWeRiE4YDTVF-cA71Poeq19h1Q0M7NhnMEs/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyCceYsSV4Yca27eo_Nk0naLGo0RxxJ3eUc';
